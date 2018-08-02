0c5f1a0678a57d8ee1633bd60eb892583cf956ef
Kamen Shah <kamenshah@gmail.com>
2018-08-02 14:33:12 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,7 +133,8 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("litcoinfoundation.com", "seeder.litcoinfoundation.com")); // litcoin
+       // vSeeds.push_back(CDNSSeedData("litcoinfoundation.com", "seeder.litcoinfoundation.com")); // litcoin
+        vSeeds.push_back(CDNSSeedData("35.225.98.31", "35.225.98.31"));
 
         // guarantees the first 2 characters, when base58 encoded, are "t1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0xB8};
